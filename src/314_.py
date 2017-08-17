62a4b598bbb41b6ba1ff5c6b5934c228eadc9f07
0x306e <3so.d.na@gmail.com>
2017-08-17 22:44:01 +0900

    ダメです

--- src/314_.py
+++ src/314_.py
@@ -1,12 +1,10 @@
-import itertools
-
-
 if __name__ == '__main__':
     N = int(input())
     ans = 0  # answer
     seq = []  # temp list
     SUM = 0  # temp sum
     times = 0  # temp number of comb
+    flag = False  # flag for remainder
     for i in range(N//2):
         for j in range(i):
             seq.append(2)
@@ -15,9 +13,16 @@ if __name__ == '__main__':
         while SUM < N:
             if N - SUM == 1:
                 SUM += 1
+            elif N - SUM == 2:
+                SUM += 2
+                flag = True
             else:
                 seq.append(3)
                 SUM += 3
                 times += 1
-        ans += len(list(itertools.combinations_with_replacement(seq, times)))
+        ans += pow(2, times) * 2 if flag else pow(2, times)
+        seq.clear()
+        SUM = 0
+        times = 0
+        flag = False
     print(ans)
